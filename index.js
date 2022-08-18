bcfdbc247f606821d04c2624b1b574a19cbebad5
Ahmed Alli <muniirhaddadi1@gmail.com>
2022-08-18 10:16:11 +0300

    assignment complete

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+const divide = function(num1, num2){
+
+   let results=  num1 / num2;
+   results= 2000 /100
+   return results
+}
+
+const square = (parameter1= 2) => parameter1 * parameter1;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
